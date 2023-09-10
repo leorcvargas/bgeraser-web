b4545292f6a3e5fcfc0da95bea7fa9853cf2e33b
Leonardo Vargas <leocarvalho.vargas@gmail.com>
2023-09-10 00:29:02 -0300

    update footer

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,6 +1,6 @@
 import Box from "@mui/joy/Box";
 import Sheet from "@mui/joy/Sheet";
-import { Typography } from "@mui/joy";
+import { Link, Typography } from "@mui/joy";
 import styles from "./Footer.module.css";
 
 export default function Footer() {
@@ -16,7 +16,16 @@ export default function Footer() {
       }}
     >
       <Box sx={{ display: "flex", alignItems: "center", gap: 2 }}>
-        <Typography level="body-sm">Made with love by @leorcvargas</Typography>
+        <Typography level="body-sm">
+          Made with love by{" "}
+          <Link
+            href="https://github.com/leorcvargas"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            @leorcvargas
+          </Link>
+        </Typography>
       </Box>
     </Sheet>
   );
