432238225957b70d1ba0f401457bf3e6c65cd8dc
Leonardo Vargas <leocarvalho.vargas@gmail.com>
2023-09-14 00:16:15 -0300

    edit next config

--- next.config.js
+++ next.config.js
@@ -1,26 +1,15 @@
-const images =
-  process.env.NODE_ENV === "development"
-    ? {
-        remotePatterns: [
-          // {
-          //   protocol: "http",
-          //   hostname: "127.0.0.1",
-          //   port: "8080",
-          //   pathname: "/i/*",
-          // },
-          {
-            protocol: "https",
-            hostname: "bgeraser-dev.s3.amazonaws.com",
-            port: "",
-            pathname: "/**",
-          },
-        ],
-      }
-    : {};
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "bgeraser-dev.s3.amazonaws.com",
+        port: "",
+        pathname: "/**",
+      },
+    ],
+  },
   experimental: {
     serverActions: true,
   },
